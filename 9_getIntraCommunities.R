fe2aaf26f62e1f5b64d5d5df45b5ad8d4fd009b8
Diana G <dgarcia@inmegen.edu.gob.mx>
2020-02-07 17:30:28 -0600

    Added getComInfo function to get info about the  network communities

--- 9_getIntraCommunities.R
+++ 9_getIntraCommunities.R
@@ -4,6 +4,7 @@
 ### 1_getIntraInteractions.R script
 library(igraph)
 library(data.table)
+library(dplyr)
 
 types <- c("utero", "kidney", "colon", "tiroides", "lung")
 
@@ -14,19 +15,45 @@ for(type in types) {
   conds <- c("healthy", "cancer")
   
   m <- lapply(conds, function(cond) {
-    mi_data <- fread(file = paste0("networks/network-tables/", type, "-", cond, "-interactions.tsv"), 
-                    header = T, sep = "\t")
-    mi_data <- mi_data[mi_data$interaction_type == "Intra-Cytoband" | 
-                         mi_data$interaction_type == "Inter-Cytoband", ]
-    colnames(mi_data)[1:2] <- c("from", "to")
-    vertices <- union(mi_data$from, mi_data$to)
-      net <- graph_from_data_frame(mi_data, 
+    interactions <- fread(file = paste0("networks/network-tables/", type, "-", cond, "-interactions.tsv"), 
+                          header = T, sep = "\t")
+    interactions <- interactions %>% filter(interaction_type == "Intra-Cytoband" | 
+                                              interaction_type == "Inter-Cytoband")
+    
+    vertices <- fread(file = paste0("networks/network-tables/", type, "-", cond, "-vertices.tsv"), 
+                      header = T, sep = "\t")
+    vertices <- vertices %>% filter(ensemblID %in% union(interactions$source, interactions$target))
+    
+    colnames(interactions)[1:2] <- c("from", "to")
+    net <- graph_from_data_frame(interactions, 
                                  directed=F, vertices = vertices)
     comm <- cluster_infomap(graph = net, nb.trials = 10)
+    names(comm$membership) <- comm$names
+    
     df_comm <- data.frame(comm$names, comm$membership)
-    colnames(df_comm) <- c("gene", "community")
+    colnames(df_comm) <- c("ensemblID", "community")
     fwrite(df_comm, paste0("networks/network-tables/", type, "-", cond, 
                            "-communities.tsv"), sep = "\t")
     
+    comm_info <- getComInfo(comm$membership, net)
+    fwrite(comm_info, paste0("networks/network-tables/", type, "-", cond, 
+                           "-communities-info.tsv"), sep = "\t")
   })
+}
+
+getComInfo <- function(cmembership, network){
+  comp.info <- lapply(unique(cmembership), function(idc){
+    mem <- names(cmembership[cmembership == idc])
+    com <- induced.subgraph(network, mem)
+    prs <- page.rank(com)
+    chrs <- table(V(com)$chr)
+    return(data.frame(com_id = idc,
+                      pg_gene = names(which.max(prs$vector))[1], 
+                      chr = names(which.max(chrs))[1], order = length(V(com)), 
+                      size = length(E(com))))
+  })
+  comp.info <- plyr::compact(comp.info)
+  comp.info <- plyr::ldply(comp.info)
+  comp.info <- comp.info %>% arrange(desc(size))
+  return(comp.info)
 }

454bc445bf7c1b163aaaf5ff797517149ac5e898
Diana Garcia <dgarcia@inmegen.edu.mx>
2022-02-14 16:00:26 -0600

    Updating scripts and rules to get communities summary per tissue and assort enrich plot with logfc

--- workflow/rules/assortativity.smk
+++ workflow/rules/assortativity.smk
@@ -1,7 +1,7 @@
 rule done_assortativity_plots:
     input:
-        config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"_plots/assortativity/cancer-comm-assort-enrich-0_terms-{cutoff}.png",
-        config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"_plots/assortativity/normal-comm-assort-enrich-0_terms-{cutoff}.png",
+        config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"_plots/assortativity/cancer-comm-assort-enrich-{cutoff}.png",
+        config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"_plots/assortativity/normal-comm-assort-enrich-{cutoff}.png",
     output:
         config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"_plots/assortativity/assort-{cutoff}.txt",
     shell:
@@ -15,15 +15,13 @@ rule get_assortativities_plot:
         vertices=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/{cond}-vertices-{cutoff}.tsv",
         enrich=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/enrichments/go-{cond}-comm-all-{cutoff}.tsv"
     output:
-        comm_assortativity_nterms=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/assortativity/{cond}-comm-assort-enrich-{terms}_terms_only-{cutoff}.tsv",
-        comm_assortativity_tsv=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/assortativity/{cond}-comm-assort-enrich-{terms}_terms_all-{cutoff}.tsv",
-        comm_assortativity_png=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"_plots/assortativity/{cond}-comm-assort-enrich-{terms}_terms-{cutoff}.png",
+        comm_summary=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/assortativity/{cond}-comm-summary-{cutoff}.tsv",
+        comm_assortativity_png=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"_plots/assortativity/{cond}-comm-assort-enrich-{cutoff}.png",
     params:
         tissue="{tissue}",
-        cond="{cond}",
-        terms="{terms}"
+        cond="{cond}"
     log:
-        config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/log/{cond}_assortativity_plot_{terms}_terms_{cutoff}.log"
+        config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/log/{cond}_assortativity_plot_{cutoff}.log"
     script:
         "../scripts/assortativityEnrichmentPlot.R"
 
@@ -33,8 +31,9 @@ rule get_assortativities:
         membership=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/communities/{cond}-comm-all-louvain-{cutoff}.tsv",
         expression=getDEGFile
     output:
-        chr_assortativity=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/assortativity/{cond}-chr-assortativity-{cutoff}.tsv",
+    	chr_assortativity=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/assortativity/{cond}-chr-assortativity-{cutoff}.tsv",
         expr_assortativity=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/assortativity/{cond}-expr-assortativity-{cutoff}.tsv",
+        diff_expr_summary=config["datadir"]+"/{tissue}/"+config["netdir"]+"_"+config["algorithm"]+"/assortativity/{cond}-diff-expr-summary-{cutoff}.tsv",
     params:
         cond="{cond}"
     log:
--- workflow/scripts/assortativityEnrichmentPlot.R
+++ workflow/scripts/assortativityEnrichmentPlot.R
@@ -6,13 +6,11 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
-library(RColorBrewer)
 library(scales)
 
 cat("Reading files\n")
 TISSUE <- snakemake@params[["tissue"]]
 COND <- snakemake@params[["cond"]]
-TERMS <- as.integer(snakemake@params[["terms"]])
 
 substring(TISSUE, 1, 1) <- toupper(substring(TISSUE, 1, 1))
                                    
@@ -33,13 +31,13 @@ if(COND == "cancer") {
 cat("Getting communities info\n")
 comm_info <- read_tsv(snakemake@input[["comm_info"]], 
                       col_types = cols_only(com_id = col_double(), order = col_double(), 
-                                            pg_gene = col_character())) %>%
+                              pg_gene = col_character(), intra_fraction = col_double())) %>%
     left_join(read_tsv(snakemake@input[["vertices"]],
                      col_types = cols_only(ensembl = col_character(), symbol = col_character())),
             by = c("pg_gene" = "ensembl"))
 
 cat("Getting enrichments\n")
-comm_enrich <- read_tsv(snakemake@input[["enrich"]], col_types = cols( p_adjust = col_double())) %>%
+comm_enrich <- read_tsv(snakemake@input[["enrich"]], col_types = cols(p_adjust = col_double())) %>%
   filter(p_adjust < 1e-10) %>%
   select(id, commun) %>% group_by(commun) %>% 
   tally(name = "nterms") 
@@ -49,62 +47,60 @@ plot <- assort %>%
   inner_join(comm_info, by = c("community_id" = "com_id")) %>%
   left_join(comm_enrich, by = c("community_id" = "commun")) 
 
-plot[is.na(plot$nterms), "nterms"] <- 0
-
+plot[is.na(plot)] <- 0
 cat("Writing plot data\n")
 
-plot %>% filter(nterms > TERMS) %>% 
-  dplyr::select(community_id) %>% write_tsv(snakemake@output[["comm_assortativity_nterms"]])
 
 if(COND == "cancer") {
-  plot %>% select(community_id, symbol, order, diffraction_chr, diffraction_exp, mean_log_fc, nterms) %>%
-    rename(size = order, name = symbol, chr_assortativity = diffraction_chr, 
-           exp_assprtativity = diffraction_exp, enriched_terms = nterms) %>% 
-    write_tsv(snakemake@output[["comm_assortativity_tsv"]])
+  plot %>% select(community_id, name = symbol, size = order, intra_fraction, 
+                  chr_assortativity = diffraction_chr, exp_assprtativity = diffraction_exp, 
+                  mean_log_fc, enriched_terms = nterms) %>%
+    write_tsv(snakemake@output[["comm_summary"]])
 } else {
-  plot %>% select(community_id, symbol, order, diffraction_chr, nterms) %>%
-    rename(size = order, name = symbol, chr_assortativity = diffraction_chr,
-           enriched_terms = nterms) %>% 
-    write_tsv(snakemake@output[["comm_assortativity_tsv"]])
+  plot %>% select(community_id, name = symbol, size = order, intra_fraction,
+                  chr_assortativity = diffraction_chr, enriched_terms = nterms) %>%
+    write_tsv(snakemake@output[["comm_summary"]])
 }
 
+plot <- plot %>% filter(nterms > 0)
 if(COND == "cancer") {
   cat("Building plot\n")
-  p <- ggplot(plot, aes(x = diffraction_chr, y = diffraction_exp)) + 
-    geom_point(aes(size = order, color = nterms)) +
-    geom_text(aes(label = ifelse(nterms > TERMS, as.character(symbol), NA)),
-              colour = "black", size = 1.3, check_overlap = FALSE, fontface = "bold") +
+  p <- ggplot(plot, aes(x = diffraction_chr, y = diffraction_exp, label = symbol)) + 
+    geom_point(aes(size = pmax(pmin(order, 5),200) , fill = mean_log_fc), 
+               color = "darkgrey", shape = 21) +
+    geom_text(color = "black", size = 1.3, check_overlap = FALSE, fontface = "bold") +
     geom_hline(yintercept = 0.0, linetype="dashed", color = "gray") +
     geom_vline(xintercept = 0.0, linetype="dashed", color = "gray") +
     theme_base() +
-    labs(size = "Nodes in \ncommunity",
-         color = "Enriched \nterms") +
-    xlab("Chromosomal assortativity") +
-    ylab("Expression assortativity") +
-    scale_color_gradient(low="lightblue1", high="steelblue4", breaks = c(0, 10, 20, 30),
-                         limits = c(0, 30), oob = squish) +
-    scale_size_continuous(range = c(1, 7), breaks = c(1, 5, 20, 50, 100, 150, 200)) +
+    xlim(c(-1, 1)) +
+    ylim(c(-1, 1)) +
+    labs(size = "Nodes in \ncommunity", fill = "Mean \nLogFC", 
+         x = "Chromosomal assortativity", 
+         y = "Expression assortativity") +
+    scale_fill_gradient2(low = "#16296B", high = "#660000", limits = c(-2, 2),
+                          oob = scales::squish) +
+    scale_size(range = c(1, 6), breaks = c(5, 50, 100, 200),
+                          limits = c(5, 200)) +
     theme(text = element_text(size = 6), 
           axis.title = element_text(size = 6),
           legend.text = element_text(size = 4),
           plot.background=element_blank(), legend.key.size = unit(0.6, "line"),
-          legend.spacing.y = unit(0.05, 'cm')) +
+          legend.spacing.y = unit(0.1, 'cm')) +
     ggtitle(TISSUE)
 } else {
-  
-  p <- ggplot(plot, aes(x = community_id, y = diffraction_chr)) + 
-    geom_point(aes(color = nterms, size = order)) +
+  p <- ggplot(plot, aes(x = community_id, y = diffraction_chr, label = symbol)) + 
+    geom_point(aes(color = nterms, size = pmax(pmin(order, 5),200))) +
     ylab("Chromosomal assortativity") +
     xlab("") +
+    ylim(c(-1, 1)) +
     geom_hline(yintercept = 0.0, linetype="dashed", color = "gray") +
     labs(size = "Nodes in \ncommunity",
          color = "Enriched \nterms") +
-    geom_text(aes(label = ifelse(nterms > TERMS, as.character(symbol), NA)),
-              colour = "black", size = 1.3, check_overlap = FALSE, fontface = "bold") +
+    geom_text(colour = "black", size = 1.3, check_overlap = FALSE, fontface = "bold") +
     scale_color_gradient(low="lightblue1", high="steelblue4", breaks = c(0, 10, 20, 30),
                          limits = c(0, 30), oob = squish) +
-    scale_size_continuous(range = c(1, 7), breaks = c(1, 5, 20, 50, 100, 150, 200)) +
-   theme_base() +
+    scale_size(range = c(1, 6), breaks = c(5, 50, 100, 200), limits = c(5, 200)) +
+    theme_base() +
     theme(text = element_text(size = 6), 
           axis.title = element_text(size = 6),
           legend.text = element_text(size = 4),
@@ -113,6 +109,7 @@ if(COND == "cancer") {
           plot.background = element_blank(), legend.key.size = unit(0.6, "line"),
           legend.spacing.y = unit(0.05, 'cm')) +
     ggtitle(TISSUE)
+  
 }
 cat("Saving plot\n")
 png(snakemake@output[["comm_assortativity_png"]], 
